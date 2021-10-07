0e7ca99f5b240690ad0d4fb3617ce5fc64e7b2ae
Alex Grisales <viga.23@hotmail.com>
2021-10-06 21:52:01 -0500

    feat: add header and footer component

---
+++ src/components/footer/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex bg-gray-800 bg-opacity-60 text-white p-2">
+    <p>Desarrollado por: <b>Alver Grisales</b></p>
+  </div>
+</template>
+
+<script src="./_index.ts" lang="ts"></script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <article v-for="character in characters" :key="character.id">
+      <img :src="character.image" :alt="character.name" />
+      <p>{{ character.name }}</p>
+    </article>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import { Context } from "@nuxt/types";
 
-export default Vue.extend({})
+export default Vue.extend({
+  async asyncData(ctx: Context): Promise<void | object> {
+    const { results } = await ctx.$axios.$get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    return {
+      characters: results,
+    };
+  },
+  data: () => ({
+    characters: [],
+  }),
+});
 </script>
